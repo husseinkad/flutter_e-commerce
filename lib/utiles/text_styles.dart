2d02163ae61fc0ac30a3b8f18fa5dbb2d0225f26
xbanker3 <72931678+xbanker3@users.noreply.github.com>
2022-12-22 17:09:24 +0300

    Adding fonts

--- lib/utiles/text_styles.dart
+++ lib/utiles/text_styles.dart
@@ -3,7 +3,7 @@ import 'package:task_flutter/utiles/colors.dart';
 
 
 
-TextStyle boldTitleStyle = TextStyle(fontSize: 18, color: customBlack, fontWeight: FontWeight.bold);
-TextStyle smallBoldStyle = TextStyle(fontSize: 15, color: customBlack, fontWeight: FontWeight.bold);
-TextStyle normalStyle = TextStyle(fontSize: 14, color: customBlack,);
-TextStyle hintGreyStyle = TextStyle(fontSize: 14, color: hintGrey,);
+TextStyle boldTitleStyle = TextStyle(fontSize: 18, color: customBlack, fontWeight: FontWeight.bold, fontFamily: 'bev');
+TextStyle smallBoldStyle = TextStyle(fontSize: 15, color: customBlack, fontWeight: FontWeight.bold, fontFamily: 'bev');
+TextStyle normalStyle = TextStyle(fontSize: 14, color: customBlack, fontFamily: 'bev');
+TextStyle hintGreyStyle = TextStyle(fontSize: 14, color: hintGrey, fontFamily: 'bev');
