faba160a136cfe41775053c843e541ea020ebe7f
xbanker3 <72931678+xbanker3@users.noreply.github.com>
2022-12-23 01:17:50 +0300

    changing the size font and adding a font family to the styles

--- lib/utiles/text_styles.dart
+++ lib/utiles/text_styles.dart
@@ -3,7 +3,7 @@ import 'package:task_flutter/utiles/colors.dart';
 
 
 
-TextStyle boldTitleStyle = TextStyle(fontSize: 18, color: customBlack, fontWeight: FontWeight.bold, fontFamily: 'bev');
-TextStyle smallBoldStyle = TextStyle(fontSize: 15, color: customBlack, fontWeight: FontWeight.bold, fontFamily: 'bev');
-TextStyle normalStyle = TextStyle(fontSize: 14, color: customBlack, fontFamily: 'bev');
-TextStyle hintGreyStyle = TextStyle(fontSize: 14, color: hintGrey, fontFamily: 'bev');
+TextStyle boldTitleStyle = TextStyle(fontSize: 17, color: customBlack, fontWeight: FontWeight.bold, fontFamily: 'bev');
+TextStyle smallBoldStyle = TextStyle(fontSize: 14, color: customBlack, fontWeight: FontWeight.bold, fontFamily: 'bev');
+TextStyle normalStyle = TextStyle(fontSize: 13, color: customBlack, fontFamily: 'bev');
+TextStyle hintGreyStyle = TextStyle(fontSize: 13, color: hintGrey, fontFamily: 'bev');
